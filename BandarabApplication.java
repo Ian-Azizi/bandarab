ffb4e099cd4dccaa344a16c42199de5b7c1566eb
Amiryasin Azizi <90372346+Ian-Azizi@users.noreply.github.com>
2023-07-24 09:41:15 +0330

    main file
    main file for run the project (DO NOT CHANGE IT)

---
+++ BandarabApplication.java
@@ -0,0 +1,13 @@
+package Bandarab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandarabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandarabApplication.class, args);
+	}
+
+}
